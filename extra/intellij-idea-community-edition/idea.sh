6e38ab6e454adb5083d341d1de62136d9c5e7d7a
gripped <gripped@gripped.com>
2024-08-29 02:13:19 +0100

    Updated intellij-idea-community-edition 4-2024.2.0.2-2

--- extra/intellij-idea-community-edition/idea.sh
+++ extra/intellij-idea-community-edition/idea.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ -z "$IDEA_JDK" ] ; then
-  IDEA_JDK="/usr/lib/jvm/java-17-openjdk/"
+  IDEA_JDK="/usr/lib/jvm/java-21-openjdk/"
 fi
 # open-jfx location that should match the JDK version
 if [ -z "$IDEA_JFX" ] ; then
-  IDEA_JFX="/usr/lib/jvm/java-17-openjfx/"
+  IDEA_JFX="/usr/lib/jvm/java-21-openjfx/"
 fi
 # classpath according to defined JDK/JFX
 if [ -z "$IDEA_CLASSPATH" ] ; then
