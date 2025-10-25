9fc96b13a9b46cdfc3cfc52adddcae7575b83feb
gripped <gripped@gripped.com>
2025-10-25 03:37:01 +0100

    Updated mediathekview 14.3.1-1

--- extra/mediathekview/mediathekview.sh
+++ extra/mediathekview/mediathekview.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+set -euo pipefail
+
+# Setup data directory to be compliant with the XDG Base Directory specification.
+# Migrate an existing, non-compliant data directory, if possible.
+data_directory="${XDG_DATA_HOME:-$HOME/.local/share}/mediathekview"
+upstream_default="$HOME/.mediathek3"
+if [[ -d "$upstream_default" ]] && [[ ! -d "$data_directory" ]]; then
+  # migrate existing data
+  mv -v "$upstream_default" "$data_directory"
+fi
 
 java_options=(
   # the application enforces the use of certain JVM parameters:
-  # https://github.com/mediathekview/MediathekView/blob/14.0.0/src/main/java/mediathek/Main.java#L286
+  # https://github.com/mediathekview/MediathekView/blob/14.3.0/src/main/java/mediathek/Main.java#L282
   -XX:+UseShenandoahGC
   -XX:ShenandoahGCHeuristics=compact
   -XX:+UseStringDeduplication
   -XX:MaxRAMPercentage=25  # use the default 25%
   --add-opens=java.desktop/sun.awt.X11=ALL-UNNAMED
+  --add-exports=java.desktop/sun.swing=ALL-UNNAMED
+  --enable-native-access=ALL-UNNAMED
+  --add-modules jdk.incubator.vector
   -DexternalUpdateCheck  # disable automatic update check
   -jar /usr/share/java/mediathekview/MediathekView.jar
 )
 PATH="/usr/lib/jvm/java-JAVA_VERSION-openjdk/bin:${PATH}"
 
-exec java "${java_options[@]}" "$@"
+exec java "${java_options[@]}" "$@" "$data_directory"
