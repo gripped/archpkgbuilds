3f5fc5a8257ab76f3ffdbc08c71cd1b4c94404d4
gripped <gripped@gripped.com>
2026-02-02 15:11:29 +0000

    Updated ex-vi-compat 2-1

--- extra/ex-vi-compat/vi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-#
-# Calls `vim` using its `vi` compatibility mode (`-C`).
-# Ignores `vim` configuration files (`-u NONE`) as `vim` specifics are not supported in `vi` anyways.
-# Accepts further options.
-
-vim -C -u NONE "$@"
