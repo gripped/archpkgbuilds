e9ba5cabeeb6d5969d2bae9cb01239dd1727cb67
gripped <gripped@gripped.com>
2026-05-04 04:34:18 +0100

    Updated tt-rss 2-r12842.f88091ccc-1

--- extra/tt-rss/config.php
+++ extra/tt-rss/config.php
@@ -4,5 +4,8 @@
 // Specify them before including user config, so that they can be overridden
 putenv('TTRSS_LOCK_DIRECTORY=/var/lib/tt-rss/lock');
 putenv('TTRSS_CACHE_DIR=/var/lib/tt-rss/cache');
+putenv('TTRSS_LOCAL_PLUGINS_DIR=/var/lib/tt-rss/plugins.local');
+putenv('TTRSS_LOCAL_TEMPLATES_DIR=/var/lib/tt-rss/templates.local');
+putenv('TTRSS_LOCAL_THEMES_DIR=/var/lib/tt-rss/themes.local');
 
 require_once '/etc/webapps/tt-rss/config.php';
