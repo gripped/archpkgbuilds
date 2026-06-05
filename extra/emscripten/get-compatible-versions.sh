01546724c54e1d454bfc93063aa91601089d652c
gripped <gripped@gripped.com>
2026-06-05 02:32:00 +0100

    Updated emscripten 6.0.0-1

--- extra/emscripten/get-compatible-versions.sh
+++ extra/emscripten/get-compatible-versions.sh
@@ -6,6 +6,7 @@
 # Change pkgver in PKGBUILD to desired version and then run ./get-compatible-versions.sh.
 # This will fetch, print and substitute into the PKGBUILD the compatible binaryen and llvm versions.
 
+updpkgsums  # To download the package sources
 pkgver=$(makepkg --printsrcinfo | sed -rn 's/.*pkgver = (.*)/\1/gp')
 tag_hash=$(curl -Ls https://github.com/emscripten-core/emsdk/raw/main/emscripten-releases-tags.json | jq ".releases[\"$pkgver\"]" | sed s/\"//g)
 deps_file=$(curl -Ls "https://chromium.googlesource.com/emscripten-releases/+/$tag_hash/DEPS?format=TEXT" | base64 -d)
@@ -13,3 +14,4 @@ binaryen_revision=$(echo "$deps_file" | sed -rn "s/.*'binaryen_revision': '(.*)'
 llvm_project_revision=$(echo "$deps_file" | sed -rn "s/.*'llvm_project_revision': '(.*)',/\\1/gp")
 sed -i "s/_binaryen_revision=.*/_binaryen_revision=$binaryen_revision/g" PKGBUILD
 sed -i "s/_llvm_project_revision=.*/_llvm_project_revision=$llvm_project_revision/g" PKGBUILD
+updpkgsums  # Get checksums of the related repos
